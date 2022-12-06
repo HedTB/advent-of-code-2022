9e6787a698d993f1edd61bbda2da60b4db43977e
HedTB <hugobasq@gmail.com>
2022-12-06 19:04:43 +0100

    day 3

--- 3/main.py
+++ 3/main.py
@@ -1,4 +1,4 @@
-import string
+import string, itertools
 
 
 with open("3/input.txt", "r") as file:
@@ -9,16 +9,24 @@ def part_1() -> int:
     total_priority = 0
 
     for line in lines:
-        compartments = [line[:len(line) // 2], line[len(line) // 2:]]
+        compartments = [line[: len(line) // 2], line[len(line) // 2 :]]
 
-        for item in compartments[0]:
-            if compartments[1].find(item) != 1:
-                total_priority += string.ascii_letters.index(item) + 1
+        item = [item for item in compartments[0] if item in compartments[1]][0]
+        total_priority += string.ascii_letters.index(item) + 1
 
     return total_priority
 
+
 def part_2() -> int:
     total_priority = 0
+
+    for line_group in itertools.zip_longest(*[iter(lines)] * 3, fillvalue=""):
+        total_priority += (
+            string.ascii_letters.index(
+                (set(line_group[0].strip()) & set(line_group[1].strip()) & set(line_group[2].strip())).pop()  # type: ignore
+            )
+            + 1
+        )
 
     return total_priority
 
